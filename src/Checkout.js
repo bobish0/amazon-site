6f5ab8b1080b138866b5f9978532353e700e1812
bobish0 <kristian.a.boban@gmail.com>
2021-11-09 02:29:18 +0100

    started on Checkout component

---
+++ src/Checkout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Checkout.css';
+
+function Checkout() {
+    
+
+//    const [{ basket }] = useStateValue();
+
+
+    return (
+        <div className="checkout">
+            <h2> This is the checkout page</h2>
+        </div>
+    )
+}
+
+export default Checkout;
+
